7d426b948cb67dc7578a195bd92d089c1a09efaa
Shaddox <sorin.gears@gmail.com>
2022-06-30 21:45:40 +0300

    add infinite loop

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import time
 
 import mercari
 import yaml
@@ -23,17 +25,23 @@ if __name__ == '__main__':
     dbConnection = database.create_connection(config['database_name'])
     dbCursor = database.get_cursor(dbConnection)
     database.provision_db(dbConnection, dbCursor)
-    for keyword in config['keywords']:
-        print(keyword)
-        # currentBatchId = database.get_batch_id_for_keyword(dbConnection, dbCursor, keyword)
-        # productsForKeyword = mercari.search(keyword)
-        # database.insert_products(dbConnection, dbCursor, keyword, productsForKeyword, currentBatchId)
 
-    allNewProducts = database.find_new_products_for_all_keywords(dbCursor)
-    print(allNewProducts)
-    if config["email"]['want'] and allNewProducts:
-        email_manager.build_and_send_email_with_new_stuff(config['email'], allNewProducts)
-    if config["logs"]["want"] and allNewProducts:
-        html_logger.create_html_log(allNewProducts, config["logs"]["path"])
+    while True:
+        breakBetweenKeywords = random.randint(config['lower_bound_keyword_search'], config['upper_bound_keyword_search'])
+        for keyword in config['keywords']:
+            print(keyword)
+            currentBatchId = database.get_batch_id_for_keyword(dbConnection, dbCursor, keyword)
+            productsForKeyword = mercari.search(keyword)
+            database.insert_products(dbConnection, dbCursor, keyword, productsForKeyword, currentBatchId)
+            time.sleep(breakBetweenKeywords)
 
-    print("Iteration ended")
+        allNewProducts = database.find_new_products_for_all_keywords(dbCursor)
+        print(allNewProducts)
+        if config["email"]['want'] and allNewProducts:
+            email_manager.build_and_send_email_with_new_stuff(config['email'], allNewProducts)
+        if config["logs"]["want"] and allNewProducts:
+            html_logger.create_html_log(allNewProducts, config["logs"]["path"])
+        print("Iteration ended")
+        breakBetweenIterations = random.randint(config['lower_bound_search'], config['upper_bound_search'])
+        time.sleep(breakBetweenIterations)
+
